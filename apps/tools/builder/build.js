f328646646bb69848bdc4c4e442498558b5ba26a
xuld <xuld@vip.qq.com>
2013-01-16 10:13:12 +0800

    导入以前的数据

---
+++ apps/tools/builder/build.js
@@ -0,0 +1,17 @@
+var Builder = require('../builder/builder');
+var Configs = require('../configs/build');
+var Path = require('path');
+
+var builder = new Builder();
+
+// 将配置文件中的 from 和 to 统一改为绝对路径。
+var rootPath = require('./configs').rootPath;
+
+if (Configs.from)
+	Configs.from = Path.resolve(rootPath, Configs.from);
+
+if (Configs.to)
+	Configs.to = Path.resolve(rootPath, Configs.to);
+
+builder.loadConfigs(Configs);
+builder.build("/", ".html");
