f824b0cebe8c24e3ff6ab37745ecfef7ccfd65de
xuld <xuld@vip.qq.com>
2013-01-16 17:37:49 +0800

    组件合成

--- apps/tools/builder/build.js
+++ apps/tools/builder/build.js
@@ -1,17 +1,29 @@
-var Builder = require('../builder/builder');
-var Configs = require('../configs/build');
-var Path = require('path');
+var Bpm = require('bpm/lib');
 
-var builder = new Builder();
+var buildSrc = process.argv[2];
 
-// 将配置文件中的 from 和 to 统一改为绝对路径。
-var rootPath = require('./configs').rootPath;
+if (buildSrc) {
 
-if (Configs.from)
-	Configs.from = Path.resolve(rootPath, Configs.from);
 
-if (Configs.to)
-	Configs.to = Path.resolve(rootPath, Configs.to);
 
-builder.loadConfigs(Configs);
-builder.build("/", ".html");
+}
+
+console.log(buildSrc)
+
+// var Builder = require('../builder/builder');
+//var Configs = require('../configs/build');
+//var Path = require('path');
+
+//var builder = new Builder();
+
+//// 将配置文件中的 from 和 to 统一改为绝对路径。
+//var rootPath = require('./configs').rootPath;
+
+//if (Configs.from)
+//	Configs.from = Path.resolve(rootPath, Configs.from);
+
+//if (Configs.to)
+//	Configs.to = Path.resolve(rootPath, Configs.to);
+
+//builder.loadConfigs(Configs);
+//builder.build("/", ".html");
